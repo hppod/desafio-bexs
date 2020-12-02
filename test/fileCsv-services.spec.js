4802bb0e8f81907f8b2e873b04a4d1f20abf2d83
hppod <hppodgornik@gmail.com>
2020-12-01 21:25:27 -0300

    Corrigindo nome do arquivo de testes

--- test/fileCsv-services.spec.js
+++ test/fileCsv-services.spec.js
@@ -1,12 +1,12 @@
 const chai = require('chai')
 const http = require('chai-http')
 const subset = require('chai-subset')
-const fileCsv = require('./../src/services/file-csv')
+const fileCsv = require('../src/services/file-csv')
 
 chai.use(http)
 chai.use(subset)
 
-describe('Function tests', () => {
+describe('File CSV Services Functions', () => {
     it('load', () => {
         const loaded = fileCsv.load('input-test.csv')
         chai.expect(loaded).to.equal(true)
